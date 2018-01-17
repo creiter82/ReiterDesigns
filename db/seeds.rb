2c64fd5513ce7c788a8a5adac7676cf8dde74d7d
Cameron Reiter <reiter303@hotmail.com>
2018-01-16 20:32:56 -0700

    Add styling to products index page and product seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 6.times do |product|
   Product.create!(
     title: "Product #{product}",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    price: 10.75
   )
 end
