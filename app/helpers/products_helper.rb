f1fe18fabe58672bf2a09bad72e10f5e92f54e1d
Cameron Reiter <reiter303@hotmail.com>
2018-01-24 16:49:16 -0700

    Refactor products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,12 @@
 module ProductsHelper
+
+  def feature_image product
+    @featured = product.product_images.find_by(featured: true)
+    if product.product_images.empty? 
+      link_to holder_tag('328x200'), product_path(product) 
+    else 
+      link_to image_tag(@featured.image, width: '100%'), product_path(product)  
+    end 
+  end
+
 end
